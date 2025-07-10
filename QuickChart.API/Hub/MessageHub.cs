82673ff41961a7c98eb291b2a84bab8306084610
Rasel Kabir <md.rasel@technonext.com>
2025-07-10 19:48:31 +0600

    ++ Create client project

--- QuickChart.API/Hub/MessageHub.cs
+++ QuickChart.API/Hub/MessageHub.cs
@@ -1,19 +1,56 @@
-﻿namespace QuickChart.API.Hub
+﻿using Microsoft.AspNetCore.SignalR;
+using QuickChart.API.Domain.Dto;
+
+namespace QuickChart.API.Hub
 {
     public class MessageHub: Microsoft.AspNetCore.SignalR.Hub
     {
-        public MessageHub()
+        private readonly IDictionary<string, UserRoomConnection> _connection;
+        public MessageHub(IDictionary<string, UserRoomConnection> connection)
         {
-                
+            _connection = connection ?? throw new ArgumentNullException(nameof(connection));
         }
         public override async Task OnConnectedAsync()
         {
+            var userConnection = new UserRoomConnection
+            {
+                User = Context.User?.Identity?.Name ?? "Anonymous",
+                Room = Context.GetHttpContext()?.Request.Query["room"].ToString()
+            };
+            var connectionId = Context.ConnectionId;
+            Console.WriteLine($"Client connected: {Context.ConnectionId}");
             await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
+            if (_connection.TryGetValue(Context.ConnectionId, out var userRoomConnection))
+            {
+                await Clients.Group(userRoomConnection.Room!).SendAsync("ReceiveMessage", "Lets Program Bot", $"{userRoomConnection.User} has Left the Group", DateTime.Now);
+                _connection.Remove(Context.ConnectionId);
+                await SendConnectedUsers(userRoomConnection.Room!);
+            }
             await base.OnDisconnectedAsync(exception);
+        }
+        public async Task JoinRoom(UserRoomConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room!);
+            _connection[Context.ConnectionId] = userConnection;
+
+            await Clients.Group(userConnection.Room!).SendAsync("ReceiveMessage", "Lets Program Bot", $"{userConnection.User} has Joined the Group", DateTime.Now);
+            await SendConnectedUsers(userConnection.Room!);
+        }
+        public async Task SendMessage(string message)
+        {
+            if (_connection.TryGetValue(Context.ConnectionId, out var userRoomConnection))
+            {
+                await Clients.Group(userRoomConnection.Room!).SendAsync("ReceiveMessage", userRoomConnection.User, message, DateTime.Now);
+            }
+        }
+        public Task SendConnectedUsers(string room) 
+        {
+            var users = _connection.Where(x => x.Value.Room == room).Select(x => x.Value.User).ToList();
+            return Clients.Group(room).SendAsync("ReceiveConnectedUsers", users); //ConnectedUser
         }
     }
 }
