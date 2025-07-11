eafdec9ba04d64ff6020f5779d26895e52a403c0
Rasel Kabir <md.rasel@technonext.com>
2025-07-11 17:30:07 +0600

    ++ Create welcome page for App

--- QuickChat.Client/src/app/app.routes.ts
+++ QuickChat.Client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'join-room', pathMatch: 'full'},
+  //{path: 'join-room', component: JoinRoomComponent},
+  {path: 'welcome', component: WelcomeComponent},
+  //{path: 'chat', component: ChatComponent}
+];
