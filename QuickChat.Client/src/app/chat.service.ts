9c8c8303df304fdaeca1a8f0a89a4a6cd0d60acb
Rasel Kabir <md.rasel@technonext.com>
2025-07-12 22:17:24 +0600

    ++ bug solve for stand alone component ReactiveModule

---
+++ QuickChat.Client/src/app/chat.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+  private connection: signalR.HubConnection = new signalR.HubConnectionBuilder()
+    .withUrl("http://localhost:5000/chat")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+  public messages$ = new BehaviorSubject<any>([]);
+  public connectedUsers$ = new BehaviorSubject<string[]>([]);
+  public messages: any[] = [];
+  public users: string[] = [];
+
+  constructor() { 
+    this.connection.start()
+      .then(() => console.log("Connection started"))
+      .catch(err => console.error("Error while starting connection: ", err));
+
+    this.connection.on("ReceiveMessage", (user: string, message: string, messageTime: string)=>{
+      this.messages = [...this.messages, {user, message, messageTime} ];
+      this.messages$.next(this.messages);
+    });
+  }
+
+
+    //Join Room
+  public async joinRoom(user: string, room: string){
+    return this.connection.invoke("JoinRoom", {user, room})
+  }
+}
