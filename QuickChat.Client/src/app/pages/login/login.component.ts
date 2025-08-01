814913160c9be587ffdb17a0b323391e1da3c452
Rasel Kabir <md.rasel@technonext.com>
2025-08-01 14:43:47 +0600

    ++ feat(auth): Add user login component functionality

---
+++ QuickChat.Client/src/app/pages/login/login.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
