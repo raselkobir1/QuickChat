e30ac8efa1d43fcd6a121893be40b912a5a02620
Rasel Kabir <md.rasel@technonext.com>
2025-08-02 19:29:15 +0600

    ++ feat: working on chat service implementation.

---
+++ QuickChat.Client/src/app/services/chat.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+  private apiUrl = 'http://localhost:5000/api';
+  constructor(private http: HttpClient) { }
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('access_token');
+    return new HttpHeaders({ Authorization: `Bearer ${token}` });
+  }
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/Auth/users`, {
+      headers: this.getAuthHeaders()
+    });
+  }
+
+  getGroups(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/Chat/my-groups`, {
+      headers: this.getAuthHeaders()
+    });
+  }
+
+  getPrivateMessages(receiverId: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/chat/private-history/${receiverId}`, {
+      headers: this.getAuthHeaders()
+    });
+  }
+
+    getGroupMessages(groupId: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/chat/group-history/${groupId}`, {
+      headers: this.getAuthHeaders()
+    });
+  }
+}
